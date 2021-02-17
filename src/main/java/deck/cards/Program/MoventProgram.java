076b3cc8aef119daf6520c445aed9a43cb7940f0
Vegard Haugland <Vegard.Haugland@student.uib.no>
2021-02-17 23:36:00 +0100

    - Added MovementProgram enum with cards

---
+++ src/main/java/deck/cards/Program/MoventProgram.java
@@ -0,0 +1,28 @@
+package deck.cards.Program;
+
+public enum MoventProgram {
+    /** Move your robot in the direction it is facing the number of spaces indicated. */
+    MOVE_1(1,0), MOVE_2(2,0), MOVE_3(3,0),
+
+    /** Turn your robot 90 degrees to the right. The robot remains in its current space. */
+    TURN_RIGHT(0,90),
+
+    /** Turn your robot 90 degrees to the left. The robot remains in its current space. */
+    TURN_LEFT(0,-90),
+
+    /** Turn your robot 180 degrees so it faces the opposite direction. The robot remains in its current space. */
+    U_TURN(0,180),
+
+    /** Move your robot one space back. The robot does not change the direction it is facing. */
+    BACK_UP(-1,0),
+
+    /** Take one energy cube, and place it on your player mat. */
+    POWER_UP(0,0);
+
+    /** Repeat the programming in your previous register. If your previous register was a damage card,
+     * draw a card from the top of your deck, and play that card this register.If you used an upgrade in
+     * your previous register that allowed you to play multiple programming cards, re-execute the second card.
+     * This card cannot be played in the first register. */
+
+    MoventProgram(int movement, int rotation){}
+}
