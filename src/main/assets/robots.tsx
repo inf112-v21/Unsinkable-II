095e1b7eca799d5650dd053f97686d8e0c40575a
Inge Halvorsen <iha001@uib.no>
2021-03-01 15:06:11 +0100

    added grapics for robots

---
+++ src/main/assets/robots.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="robots" tilewidth="77" tileheight="77" margin="3" tilecount="8" columns="4">
+ <image source="../../../../../Pictures/robots.png" width="320" height="157"/>
+</tileset>
