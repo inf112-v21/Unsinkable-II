7456fc0c1545d09b0b741c4dbea24a291db2e247
Inge Halvorsen <iha001@uib.no>
2021-03-06 19:02:45 +0100

    Added enum for tilelayers, in development

---
+++ src/main/java/RoboRally/Game/Board/TileID.java
@@ -0,0 +1,33 @@
+package RoboRally.Game.Board;
+
+public enum TileID {
+    //Startpossitions
+    Start1(120),
+    Start2(121),
+    Start3(122),
+    Start4(123),
+    Start5(128),
+    Start6(129),
+    Start7(130),
+    Start0(131),
+    //Holes
+    SmallHole(5),
+    //BlueConveyer
+    BlueConveyerN(12),
+    BlueConveyerS(20),
+    BlueConveyerE(13),
+    BlueConveyerW(21),
+    BlueConveyerWS(16),
+    BlueConveyerNW(17),
+    BlueConveyerSE(24),
+    BlueConveyerEN(25),
+    BlueConveyerNE(18),
+    BlueConveyerES(19),
+    BlueConveyerWN(26),
+    BlueConveyerSW(27)
+    ;
+
+    public final int id;
+
+    TileID(int id) { this.id = id; }
+}
