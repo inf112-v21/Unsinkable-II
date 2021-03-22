637be0037f9f7e0f214f4df0829d581f5639f1d4
Inge Halvorsen <iha001@uib.no>
2021-03-22 11:45:28 +0100

    Abstracted out object from IRobot

---
+++ src/main/java/RoboRally/Game/Objects/IObject.java
@@ -0,0 +1,13 @@
+package RoboRally.Game.Objects;
+
+import com.badlogic.gdx.math.Vector2;
+
+public interface IObject {
+
+    /**
+     * @return the object's current location.
+     */
+    Vector2 getLoc();
+
+
+}
