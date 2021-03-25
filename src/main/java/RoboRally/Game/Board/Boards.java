cf503e952bf8275f6fe79a5da2c5d6e08248a222
Daniel Liland <danielsen2000@gmail.com>
2021-03-25 19:14:36 +0100

    added map for automated testing

--- src/main/java/RoboRally/Game/Board/Boards.java
+++ src/main/java/RoboRally/Game/Board/Boards.java
@@ -3,7 +3,8 @@ package RoboRally.Game.Board;
 public enum Boards {
     RISKY_EXCHANGE("Maps/RiskyExchange.tmx", "Risky Exchange"),
     CHECKMATE("Maps/Checkmate.tmx", "Checkmate"),
-    TEST("Maps/Testmap.tmx", "Test");
+    TEST("Maps/JUnitTestingMap.tmx", "Test"),
+    JUNIT_TEST_MAP("Maps/JUnitTestingMap.tmx", "JUnit_test"); // should not be included in ALL_BOARDS
 
     public static final Boards[] ALL_BOARDS = new Boards[] { RISKY_EXCHANGE, CHECKMATE, TEST };
 
