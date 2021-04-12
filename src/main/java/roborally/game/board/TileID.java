72897a83702fef110560c6fc07ca3cbe71cf50e2
Inge Halvorsen <iha001@uib.no>
2021-04-12 15:40:32 +0200

    fixed bug in TilerID

--- src/main/java/roborally/game/board/TileID.java
+++ src/main/java/roborally/game/board/TileID.java
@@ -180,7 +180,7 @@ public enum TileID {
             BELT_N.getId(),
             BELT_N_E_TURN_L.getId(),
             BELT_N_W_TURN_R.getId(),
-            BELT_N_W_TURN_R_E_TURN_L.getId(),
+            BELT_N_W_TURN_R_E_TURN_L.getId()
     );
 
     public static final Set<Integer> BELTS_SOUTH = Set.of(
